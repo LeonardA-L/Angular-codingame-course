691408289828b0dbc8648cea0c014e72e56227ba
Leonard Allain-Launay <leonard@codingame.com>
2017-03-09 15:49:03 -0800

    Up

--- root/run.sh
+++ root/run.sh
@@ -4,6 +4,6 @@ cd /project/target/
 npm install -g gulp lodash
 gulp
 echo ""
-echo "CG> open --static-dir /project/target/root/dist/index.html"
+echo "CG> open --static-dir /project/target/root/dist /index.html"
 sleep 1000
 
