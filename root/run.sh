52c2e26214646c3e0cb56315ccca5a3fe123b2b1
Leonard Allain-Launay <leonard@codingame.com>
2017-03-13 15:53:10 -0700

    Up

--- root/run.sh
+++ root/run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#npm install -g gulp lodash
 
+# Create zip file
+cd /project/target
+rm -rf /tmp/angular-project
+mkdir -p /tmp/angular-project/src
+cp -ar gulpfile.js /tmp/angular-project
+cp -ar src /tmp/angular-project/src
+cp -ar *.json /tmp/angular-project
+cd /tmp
+zip -r angular-project.zip angular-project/
+mv angular-project.zip /project/target/dist
 
 cd /project/target/
+
 gulp
 echo ""
 echo "CG> open --static-dir /project/target/dist /index.html"
