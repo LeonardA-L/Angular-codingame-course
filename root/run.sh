ac66a3b5a44e35dcd4961547acf6a3e9fc670692
Leonard Allain-Launay <leonard@codingame.com>
2017-03-13 16:15:16 -0700

    Up

--- root/run.sh
+++ root/run.sh
@@ -5,7 +5,7 @@ cd /project/target
 rm -rf /tmp/angular-project
 mkdir -p /tmp/angular-project/src
 cp -ar gulpfile.js /tmp/angular-project
-cp -ar src /tmp/angular-project/src
+cp -ar src/* /tmp/angular-project/src
 cp -ar *.json /tmp/angular-project
 cd /tmp
 zip -r angular-project.zip angular-project/
