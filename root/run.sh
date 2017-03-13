e3e00048317a1e66b122a49ef719991a450c1a2c
Leonard Allain-Launay <leonard@codingame.com>
2017-03-13 16:09:08 -0700

    Up

--- root/run.sh
+++ root/run.sh
@@ -9,10 +9,11 @@ cp -ar src /tmp/angular-project/src
 cp -ar *.json /tmp/angular-project
 cd /tmp
 zip -r angular-project.zip angular-project/
-mv angular-project.zip /project/target/dist
 
 cd /project/target/
 
 gulp
+
+mv /tmp/angular-project.zip /project/target/dist
 echo ""
 echo "CG> open --static-dir /project/target/dist /index.html"
