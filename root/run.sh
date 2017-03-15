e8395c9c06c9c3626547fbf16aa4117e4e3b2637
Leonard Allain-Launay <leonard@codingame.com>
2017-03-15 14:43:29 -0700

    Up

--- root/run.sh
+++ root/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create zip file
-cd /project/target
-rm -rf /tmp/angular-project
-mkdir -p /tmp/angular-project/src
-cp -ar gulpfile.js /tmp/angular-project
-cp -ar src/* /tmp/angular-project/src
-cp -ar *.json /tmp/angular-project
-mv README.md /tmp/angular-project
-cd /tmp
-zip -r angular-project.zip angular-project/ >/dev/null
+# cd /project/target
+# rm -rf /tmp/angular-project
+# mkdir -p /tmp/angular-project/src
+# cp -ar gulpfile.js /tmp/angular-project
+# cp -ar src/* /tmp/angular-project/src
+# cp -ar *.json /tmp/angular-project
+# mv README.md /tmp/angular-project
+# cd /tmp
+# zip -r angular-project.zip angular-project/ >/dev/null
 
 cd /project/target/
 
