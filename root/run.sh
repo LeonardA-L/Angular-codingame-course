0b055cf44ed67df24664d140de685d5cbb7fda00
Leonard Allain-Launay <leonard@codingame.com>
2017-03-15 14:49:38 -0700

    Up

--- root/run.sh
+++ root/run.sh
@@ -15,6 +15,6 @@ cd /project/target/
 
 gulp
 
-mv /tmp/angular-project.zip /project/target/dist
+# mv /tmp/angular-project.zip /project/target/dist
 echo ""
 echo "CG> open --static-dir /project/target/dist /index.html"
